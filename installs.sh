abc15b81e9e4cbc6aaacba29cae4eb1002f794af
Vianney Tran <vianney.tran@datadoghq.com>
2015-05-14 18:41:30 -0400

    I still like my theme

--- installs.sh
+++ installs.sh
@@ -2,11 +2,9 @@
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Installing OMZ"
     curl -L http://install.ohmyz.sh | zsh
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="muse"/' ./.zshrc
 	cd ~/.oh-my-zsh/custom/plugins
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 	cd ~
-	sed -i 's/plugins=(git)/plugins=(git python screen z zsh-syntax-highlighting)/' ./.zshrc
 	touch .z
 else
     echo "OMZ already installed"
