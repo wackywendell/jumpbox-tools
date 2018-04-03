20a763611b80e3d568487f992af51e53c4f6b573
Wendell Smith <wackywendell@gmail.com>
2018-04-03 12:54:55 -0400

    Update install script to do no more than necessary, which works

--- installs.sh
+++ installs.sh
@@ -1,13 +1,14 @@
 # ZSH install
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Installing OMZ"
-    curl -L http://install.ohmyz.sh | zsh
-	cd ~/.oh-my-zsh/custom/plugins
+	ZSH="$HOME/.oh-my-zsh"
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
+	# curl -L http://install.ohmyz.sh | zsh
+	cd $ZSH/custom/plugins
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 	cd ~
 	touch .z
 else
     echo "OMZ already installed"
 fi
-rm -f ~/.zshrc.pre-oh-my-zsh
 touch ~/.z
