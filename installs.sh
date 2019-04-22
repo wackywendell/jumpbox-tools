1857627bbb3bba144309ebf478b116fbade71f93
Wendell Smith <wackywendell@gmail.com>
2019-04-22 16:28:40 -0400

    Don't require git for omz

--- installs.sh
+++ installs.sh
@@ -2,10 +2,19 @@
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Installing OMZ"
 	ZSH="$HOME/.oh-my-zsh"
-    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
-	# curl -L http://install.ohmyz.sh | zsh
-	cd $ZSH/custom/plugins
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+	 if which git >/dev/null; then
+		git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
+		# curl -L http://install.ohmyz.sh | zsh
+		cd $ZSH/custom/plugins
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+	else
+		wget -q https://github.com/robbyrussell/oh-my-zsh/archive/master.zip -O omz.zip
+		unzip -q omz.zip && mv oh-my-zsh-master "$ZSH" && rm omz.zip
+
+		wget -q https://github.com/zsh-users/zsh-syntax-highlighting/archive/master.zip -O zsh-highlight.zip
+		unzip -q zsh-highlight.zip && mv zsh-syntax-highlighting-master "$ZSH/custom/plugins/zsh-syntax-highlighting" && rm zsh-highlight.zip
+
+	fi
 	cd ~
 	touch .z
 else
